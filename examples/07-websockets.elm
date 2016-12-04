0ebede2026dc69e38cf4a629d46930d0ea35f735
Steven Shaw <steven@steshaw.org>
2016-12-04 11:58:42 +1000

    Bootstrapify and handle onEnter

--- examples/07-websockets.elm
+++ examples/07-websockets.elm
@@ -1,9 +1,10 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode as Json
 import WebSocket
 
-
+import Bootstrap exposing (..)
 
 main =
   Html.program
@@ -13,72 +14,70 @@ main =
     , subscriptions = subscriptions
     }
 
-
 echoServer : String
 echoServer =
   "ws://echo.websocket.org"
 
-
-
--- MODEL
-
+-- Model
 
 type alias Model =
-  { input : String
+  { message : String
   , messages : List String
   }
-
 
 init : (Model, Cmd Msg)
 init =
   (Model "" [], Cmd.none)
 
+-- View
 
+onEnter : Msg -> Attribute Msg
+onEnter msg =
+  on "keyup" (Json.andThen (\k ->
+    if k == 13 then
+      Json.succeed msg
+    else
+      Json.fail "happily ignoring a non-enter keypress"
+  ) keyCode)
 
--- UPDATE
+view : Model -> Html Msg
+view model =
+  Bootstrap.wrap "07-websockets"
+    [ div []
+        [ input [class "form-control", onInput Input, onEnter Send, value model.message] []
+        , button [class "btn btn-secondary", onClick Send] [text "Send"]
+        , div [] (List.map viewMessage model.messages)
+        ]
+    ]
 
+viewMessage : String -> Html msg
+viewMessage msg =
+  div [] [ text msg ]
+
+-- Update
 
 type Msg
   = Input String
   | Send
   | NewMessage String
-
+  | Nothing
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg {input, messages} =
+update msg {message, messages} =
   case msg of
     Input newInput ->
       (Model newInput messages, Cmd.none)
 
     Send ->
-      (Model "" messages, WebSocket.send echoServer input)
+      (Model "" messages, WebSocket.send echoServer message)
 
     NewMessage str ->
-      (Model input (str :: messages), Cmd.none)
+      (Model message (messages ++ [str]), Cmd.none)
 
+    Nothing -> (Model message messages, Cmd.none)
 
-
--- SUBSCRIPTIONS
-
+-- Subscriptions
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   WebSocket.listen echoServer NewMessage
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ input [onInput Input] []
-    , button [onClick Send] [text "Send"]
-    , div [] (List.map viewMessage (List.reverse model.messages))
-    ]
-
-
-viewMessage : String -> Html msg
-viewMessage msg =
-  div [] [ text msg ]
