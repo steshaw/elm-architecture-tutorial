be6a8a1aa76853af7a77257df494610118aa6c7b
Steven Shaw <steven@steshaw.org>
2016-12-04 11:16:39 +1000

    Bootstrapify clock example

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -3,7 +3,7 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
 
-
+import Bootstrap exposing (..)
 
 main =
   Html.program
@@ -13,46 +13,15 @@ main =
     , subscriptions = subscriptions
     }
 
-
-
--- MODEL
-
+-- Model
 
 type alias Model = Time
-
 
 init : (Model, Cmd Msg)
 init =
   (0, Cmd.none)
 
-
-
--- UPDATE
-
-
-type Msg
-  = Tick Time
-
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-  case msg of
-    Tick newTime ->
-      (newTime, Cmd.none)
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Time.every second Tick
-
-
-
--- VIEW
-
+-- View
 
 view : Model -> Html Msg
 view model =
@@ -66,7 +35,26 @@ view model =
     handY =
       toString (50 + 40 * sin angle)
   in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+    Bootstrap.wrap "06-clock"
+      [ svg [ viewBox "0 0 100 100", width "300px" ]
+          [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+          , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+          ]
       ]
+
+-- Update
+
+type Msg
+  = Tick Time
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      (newTime, Cmd.none)
+
+-- Subscriptions
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every second Tick
